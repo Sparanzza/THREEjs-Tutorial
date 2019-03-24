1b15d3b0974e28b93b1c746dff24d179b396a3ea
Sparanzza <aurelio@sparanzza.com>
2019-03-24 15:38:06 +0100

    depth and dash material

--- src/index.js
+++ src/index.js
@@ -49,28 +49,40 @@ function main(){
     //     side: THREE.FrontSide
     // });
 
-    mt = new THREE.MeshPhongMaterial( {
-        color: 0x999999,
-        specular: 0x0000ff,
-        shininess:10,
-        side: THREE.FrontSide,
-        map: new THREE.TextureLoader().load('wood.jpg')
+    // mt = new THREE.MeshPhongMaterial( {
+    //     color: 0x999999,
+    //     specular: 0x0000ff,
+    //     shininess:10,
+    //     side: THREE.FrontSide,
+    //     map: new THREE.TextureLoader().load('wood.jpg'),
+    //     normalMap : new THREE.TextureLoader().load('wood.jpg')
+    // });
+    // mt = new THREE.MeshStandardMaterial( {
+    //     color: 0x999999,
+    //     specular: 0x0000ff,
+    //     metalness:.5,
+    //     roughness:0.3,
+    //     side: THREE.FrontSide,
+    // });
+    mt = new THREE.LineDashedMaterial({
+        dashSize:2,
+        gapSize: 2
     });
 
     // GEOMS
     // g = new THREE.SphereGeometry( 48, 16, 16 );
-    g = new THREE.BoxGeometry(40,40,40);
-    m = new THREE.Mesh( g, mt );
+    g = new THREE.BoxGeometry(64,64,64);
+    m = new THREE.Line( g, mt );
     m.position.set(-100,0,-1000);
 
-    g2 = new THREE.SphereGeometry( 32, 16, 16 );
-    m2 = new THREE.Mesh( g2, mt );
+    g2 = new THREE.SphereGeometry( 64, 16, 16 );
+    m2 = new THREE.Line( g2, mt );
     m2.position.set(100,0,-1000);
 
     // FLOOR
     let floorGeom = new THREE.PlaneGeometry(200,200,10,10);
-    let floorMesh = new THREE.Mesh( floorGeom, mt);
-    floorMesh.position.set(0, -10, -100);
+    let floorMesh = new THREE.Line( floorGeom, mt);
+    floorMesh.position.set(0, -20, -200);
     floorMesh.rotation.set( THREE.Math.degToRad(-90) ,0,0 );
 
     
@@ -94,6 +106,7 @@ function main(){
 function render(){
     m.rotation.z += 0.005;
     m.rotation.x += 0.005;
+    camera.position.z += 1;
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
