347605ac1bb2da33687bdc1b1b603840616e547f
Sparanzza <aurelio@sparanzza.com>
2019-03-27 12:25:20 +0100

    camera rotation lookAt Object

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ function main(){
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(WIDTH, HEIGHT);
     renderer.shadowMap.enabled = true;
-	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.renderReverseSided = true;
 
 
     //MATERIALS
@@ -38,7 +39,7 @@ function main(){
     boxMesh.position.set(50,0,0);
     scene.add(boxMesh);
 
-    floorGeom = new THREE.PlaneBufferGeometry( 400, 400, 32, 32 );
+    floorGeom = new THREE.PlaneBufferGeometry( 400, 400, 320, 320 );
     floorMesh = new THREE.Mesh( floorGeom, material);
     floorMesh.position.set(0, -50,0);
     floorMesh.rotation.set( THREE.Math.degToRad(-90) ,0,0 );
@@ -59,8 +60,8 @@ function main(){
     spotLight.penumbra = 0.01;
     spotLight.decay = 2;
     spotLight.castShadow = true;
-    spotLight.shadow.mapSize.width = 1024;
-    spotLight.shadow.mapSize.height = 1024;
+    spotLight.shadow.mapSize.width = 2048;
+    spotLight.shadow.mapSize.height = 2048;
     //create shadow
     //spotLight.shadow.bias = 0.001;
     scene.add(spotLight);
@@ -71,8 +72,14 @@ function main(){
     // RENDER
     render();
 }
-
+var delta = 0;
 function render() {
+    delta +=0.002;
+    camera.lookAt(boxMesh.position);
+
+    camera.position.x = Math.sin(delta)*200;
+    camera.position.z = Math.cos(delta)*200;
+
     boxMesh.rotation.z += 0.01;
     boxMesh.rotation.x += 0.01;
     renderer.render(scene, camera);
