45a2b107f7398c2a249fe909c50477c154cd525d
Sparanzza <aurelio@sparanzza.com>
2019-03-28 23:35:41 +0100

    add ignore file

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(35, WIDTH / HEIGHT, .1 ,10000);
 scene.background = new THREE.Color(0x001155);
 
+
+
 //CUBE
 let geometry = new THREE.BoxGeometry( 1, 1, 1 );
 let material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
@@ -19,14 +21,24 @@ scene.add( cube );
 camera.position.z = 5;
 camera.lookAt (cube.position);
 
+//LIGHTS
+//create Directional light
+let directionalLight = new THREE.DirectionalLight(0xffffff , 1 );
+directionalLight.target = cube;
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+//create ambient light
+let ambientLight = new THREE.AmbientLight(0xffffff , .3);
+scene.add(ambientLight);
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
-
 
 function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    camera.position.z += 0.01;
     renderer.render( scene, camera );
 	requestAnimationFrame( animate );
 }
